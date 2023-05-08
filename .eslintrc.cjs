008ecdfeb414d253779d3d07d20a3e1c70094fb9
alexravenna <alex.n.ravenna@gmail.com>
2023-05-08 22:42:49 +0200

    Add ESlint plugins and additional deps

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
