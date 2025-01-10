6e258c8c2777fa5c5adbbec42c86a9380380b05b
Alex Ravenna <alex.n.ravenna@gmail.com>
2025-01-10 22:31:40 +0100

    updated site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alexravenna.github.io',
+  site: 'https://alexravenna.is-a.dev',
   integrations: [svelte()]
 });
