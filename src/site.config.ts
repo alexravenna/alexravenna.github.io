033976b4be86b1b843cbf6d710069de40f182b66
Alex Ravenna <alex.n.ravenna@gmail.com>
2026-02-09 14:50:15 +0000

    feat: use correct site URL for GitHub Pages

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://multiterm.stelclementine.com',
+  site: 'https://alexravenna.github.io/site',
   // The name of your site, used in the title and for SEO.
   title: 'Eventually Consistent',
   // The description of your site, used for SEO and RSS feed.
